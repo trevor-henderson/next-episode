9cc327fd35a73afffff7e461bc3126d1abd7b149
trevor-henderson <trevorsgit@gmail.com>
2020-02-06 22:22:52 -0600

    initialize testing framework

---
+++ src/app/components/tool-bar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ToolBar = () => {
+    return (
+        <div>
+            <p>I'm a tool bar</p>
+        </div>
+    );
+};
+
+export default ToolBar;
---
+++ src/app/components/tool-bar.test.js
@@ -0,0 +1,7 @@
+import ToolBar from './tool-bar';
+
+describe("ToolBar", () => {
+   test("should be truthy", () => {
+      expect(ToolBar).toBeTruthy();
+   });
+});
